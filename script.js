33f9789d7f0d5478b1a0c4e9fa8e61d5582c00f4
Evan Mackay <evanmackay@evans-mbp.home>
2020-08-19 18:17:04 -0500

    started JS. Created static variables and added current time/date to be displayed

---
+++ script.js
@@ -0,0 +1,13 @@
+let currentDay = $('#currentDay')
+let timeNine = $('#time-nine')
+let timeTen = $('#time-ten')
+let timeEleven = $('#time-eleven')
+let timeTwelve = $('#time-twelve')
+let timeOne = $('#time-one')
+let timeTwo = $('#time-two')
+let timeThree = $('#time-three')
+let timeFour = $('#time-four')
+let timeFive = $('#time-five')
+
+currentDay.text(moment().format('dddd MMMM Do YYYY, h:mm a'))
+
